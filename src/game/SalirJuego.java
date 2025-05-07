b7bdfe009823ef624986aa94da0bc9056e60d4c0
Luis <lmrodriguez2017@gmail.com>
2025-05-07 04:40:50 +0200

    Implementar la interfaz Serializable en la clase SalirJuego para permitir la serialización de objetos de salida del juego.

--- src/game/SalirJuego.java
+++ src/game/SalirJuego.java
@@ -1,9 +1,12 @@
 package game;
 
+import java.io.Serializable;
+
 /**
  * Clase que maneja la salida del juego
  */
-public class SalirJuego {
+public class SalirJuego implements Serializable {
+    private static final long serialVersionUID = 1L;
     
     /**
      * Ejecuta la acción de salir del juego
@@ -12,4 +15,4 @@ public class SalirJuego {
         System.out.println("\n👋 ¡Hasta luego!");
         System.exit(0);
     }
-} 
+}
