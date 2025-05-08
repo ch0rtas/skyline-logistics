1deaf11dfe44897f000840a2922ae70d196eb704
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2025-05-08 18:41:28 +0200

    Agregar la clase CampanaObjetivos para gestionar los objetivos de campaña en diferentes niveles de dificultad. Se extrae la lógica de objetivos del juego, mejorando la organización del código y facilitando su mantenimiento.

--- src/game/JuegoLogistica.java
+++ src/game/JuegoLogistica.java
@@ -10,6 +10,7 @@ import static game.ReparacionVehiculos.repararVehiculo;
 import static game.MercadoVehiculos.mostrarMercadoVehiculos;
 import static game.EstadisticasHandler.guardarEstadisticas;
 import static game.PedidoUtils.mostrarPedidosPendientes;
+import static game.CampanaObjetivos.OBJETIVOS_CAMPANA;
 
 import java.util.Scanner;
 import java.util.Random;
@@ -62,34 +63,6 @@ public class JuegoLogistica {
     private List<Vehiculo> vehiculosMercado;
     private PedidoGenerator pedidoGenerator;
     private static final String[] TIPOS_CARGA = {"NORMAL", "REFRIGERADO", "CONGELADO", "PELIGROSO", "ESCOLTADO", "FRÁGIL", "PERECEDERO", "ALTO_VALOR", "SERES_VIVOS"};
-
-    private static final Map<String, Map<String, Integer>> OBJETIVOS_CAMPANA = new HashMap<>();
-    
-    static {
-        // Objetivos nivel fácil
-        Map<String, Integer> objetivosFacil = new HashMap<>();
-        objetivosFacil.put("dias", 30);
-        objetivosFacil.put("enviosExitosos", 100);
-        objetivosFacil.put("satisfaccion", 80);
-        objetivosFacil.put("beneficios", 100000);
-        OBJETIVOS_CAMPANA.put("facil", objetivosFacil);
-        
-        // Objetivos nivel medio
-        Map<String, Integer> objetivosMedio = new HashMap<>();
-        objetivosMedio.put("dias", 60);
-        objetivosMedio.put("enviosExitosos", 350);
-        objetivosMedio.put("satisfaccion", 90);
-        objetivosMedio.put("beneficios", 250000);
-        OBJETIVOS_CAMPANA.put("medio", objetivosMedio);
-        
-        // Objetivos nivel difícil
-        Map<String, Integer> objetivosDificil = new HashMap<>();
-        objetivosDificil.put("dias", 100);
-        objetivosDificil.put("enviosExitosos", 920);
-        objetivosDificil.put("satisfaccion", 95);
-        objetivosDificil.put("beneficios", 500000);
-        OBJETIVOS_CAMPANA.put("dificil", objetivosDificil);
-    }
 
     /**
      * Constructor del juego
