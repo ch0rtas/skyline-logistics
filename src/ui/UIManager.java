7b0ced92abe5adb89bcdc1568dfa054da9b76514
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2025-05-09 07:12:06 +0200

    Actualización de la clase `UIManager` para incluir a Manuel Martinez en la lista de desarrolladores y mejorar la presentación de los enlaces del repositorio del proyecto, añadiendo un formato numerado para mayor claridad.

--- src/ui/UIManager.java
+++ src/ui/UIManager.java
@@ -191,12 +191,13 @@ public class UIManager {
         System.out.println("Juego desarrollado en JAVA para la asignatura de");
         System.out.println("Diseño de Software por alumnos de U-Tad");
         System.out.println("\nDesarrolladores:");
-        System.out.println("• Manuel Martinez | GitHub: @ch0rtas");
         System.out.println("• Luis Marquina | GitHub: @Luiiss44");
+        System.out.println("• Manuel Martinez | GitHub: @ch0rtas");
         System.out.println("• Miguel Toran");
         System.out.println("\nRepositorio del proyecto:");
-        System.out.println("https://github.com/Luiiss44/skyline-logistics");
-        
+        System.out.println("01. https://github.com/Luiiss44/skyline-logistics");
+        System.out.println("02. https://github.com/ch0rtas/skyline-logistics [Fork]");
+
         System.out.println("\nEscribe 0 para volver al menú principal...");
         new Scanner(System.in).nextLine();
     }
